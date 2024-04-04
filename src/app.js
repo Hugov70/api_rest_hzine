c80d250fa3b0349dc2563fd3f8351ea536f3093a
Hugov70 <145301917+Hugov70@users.noreply.github.com>
2024-04-04 15:39:23 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -17,6 +17,9 @@ const whiteList = [
   'http://localhost:3000',
   'http://localhost:3001',
   'http://localhost:3002',
+  'https://hzine.netlify.app/',
+  'https://hzine.netlify.app/login/',
+  'https://hzine.netlify.app/register/'
 ]
 
 const corsOptions = {
