e76ae1cc59407325a6394161b3cbb5af2726e71c
Hugov70 <145301917+Hugov70@users.noreply.github.com>
2024-04-06 18:56:41 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -52,15 +52,13 @@ class App {
       this.app.use('/', homeRoutes);
       this.app.use('/users', userRoutes);
       this.app.use('/tokens', function(req, res, next) {
-        res.header("Access-Control-Allow-Origin", "*");
-        res.header("Access-Control-Allow-Credentials", "true");
-        res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-        if ('OPTIONS' == req.method) {
-        res.sendStatus(200);
-        } else {
-          next();
-        }
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Origin,X-Requested-With, Content-Type, Accept, Authorization"
+        );
+        res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,PUT,DELETE,OPTIONS");
+        next();
       } , tokenRoutes);
     }
   }
