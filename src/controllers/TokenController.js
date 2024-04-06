ab97b299ce990e408c9136b03e2967eb7fc18361
Hugov70 <145301917+Hugov70@users.noreply.github.com>
2024-04-06 18:16:09 -0300

    Update TokenController.js

--- src/controllers/TokenController.js
+++ src/controllers/TokenController.js
@@ -31,10 +31,10 @@ class TokenController {
       expiresIn: process.env.TOKEN_EXPIRATION,
     });
 
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.setHeader("Access-Control-Allow-Credentials", "true");
-    response.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    response.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
 
     return res.json({ token, user: {
       nome: user.nome,
