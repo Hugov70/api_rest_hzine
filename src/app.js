be45e853281179776079f65b01d1024e9e7b5af7
Hugov70 <145301917+Hugov70@users.noreply.github.com>
2024-04-06 18:38:35 -0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -55,7 +55,7 @@ class App {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Credentials", "true");
         res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-        res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
         if ('OPTIONS' == req.method) {
         res.sendStatus(200);
         } else {
